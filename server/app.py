1b30328259c45af55eeeccabc70c02cf60891f1b
lixelamos <amosbett99@gmail.com>
2023-06-23 15:32:37 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,47 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    messages_serialized = [{'id': msg.id, 'body': msg.body, 'username': msg.username, 'created_at': msg.created_at, 'updated_at': msg.updated_at} for msg in messages]
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    return jsonify(messages_serialized)
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    body = request.json.get('body')
+    username = request.json.get('username')
+
+    message = Message(body=body, username=username)
+    db.session.add(message)
+    db.session.commit()
+
+    return jsonify({'id': message.id, 'body': message.body, 'username': message.username, 'created_at': message.created_at, 'updated_at': message.updated_at})
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'message': 'Message not found'})
+
+    body = request.json.get('body')
+
+    message.body = body
+    db.session.commit()
+
+    return jsonify({'id': message.id, 'body': message.body, 'username': message.username, 'created_at': message.created_at, 'updated_at': message.updated_at})
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'message': 'Message not found'})
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return jsonify({'message': 'Message deleted successfully'})
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from datetime import datetime
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -9,6 +10,26 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
-
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255))
+    username = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __init__(self, body, username, created_at=None, updated_at=None):
+        self.body = body
+        self.username = username
+        self.created_at = created_at or datetime.now()
+        self.updated_at = updated_at or datetime.now()
+
+    def format_datetime(self, dt):
+        return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.format_datetime(self.created_at),
+            'updated_at': self.format_datetime(self.updated_at)
+        }
